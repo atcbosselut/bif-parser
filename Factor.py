774fc97c0a5851f43fbba225200f285701b3e0c9
Antoine Bosselut <antoine.bosselut@gmail.com>
2015-02-20 16:16:34 -0800

    Parser script, and Node and Factor Objects version 1.0.1

---
+++ Factor.py
@@ -0,0 +1,18 @@
+class Factor:
+	def __init__(self, table, nodes):
+		self.fields = nodes
+		self.table = table
+
+	def updateFactors(self, table):
+		print ""
+
+	def getFields(self):
+		return nodes;
+
+	def getTable(self):
+		return table;
+
+	def updateTable(self, information):
+		#TODO: Find an appropriate scheme for updating the factor table based on the information
+		self.table=table;
+
